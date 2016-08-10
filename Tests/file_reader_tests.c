dc11de464a650ad1fdd9cc5bc27e7c8fe16076d2
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2016-08-10 15:38:34 +0100

    Added test for input file with more than one character over a single line.

--- Tests/file_reader_tests.c
+++ Tests/file_reader_tests.c
@@ -5,6 +5,7 @@ static void test_file_length()
 {
 	sput_fail_unless(file_length("test_empty_file.txt") == 0, "File length of empty file should be 0");
 	sput_fail_unless(file_length("test_one_character_file.txt") == 1, "File length of file with 1 character should be 1");
+	sput_fail_unless(file_length("test_two_characters_one_line_file.txt") == 2, "File length of file with 2 characters on 1 line should be 2");
 }
 
 int run_file_reader_tests(void)
