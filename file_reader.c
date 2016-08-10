9a3738748d636a08dca73ab29547e33e37d58321
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2016-08-10 11:06:22 +0100

    Implemented stubs for file length function.

---
+++ file_reader.c
@@ -0,0 +1,6 @@
+#include "file_reader.h"
+
+long file_length(const char *filename)
+{
+	return -1;
+}
---
+++ file_reader.h
@@ -0,0 +1,6 @@
+#ifndef FILE_READER
+#define FILE_READER
+
+long file_length(const char *filename);
+
+#endif
