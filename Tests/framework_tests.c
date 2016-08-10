d61ee2ae9d0feb7e661b191352c743aff06aa834
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2016-08-10 10:44:45 +0100

    Implemented tests to exercise the sput framework.

--- Tests/framework_tests.c
+++ Tests/framework_tests.c
@@ -3,12 +3,18 @@
 
 static void test_fail_unless()
 {
-
+	/* This should fail */
+	/* sput_fail_unless(1 == 0, "1 == 0"); */
+	/* This should pass*/
+	sput_fail_unless(1 == 1, "1 == 1");
 }
 
 static void test_fail_if()
 {
-
+	/* This should fail */
+	/* sput_fail_if(1 == 1, "1 == 1"); */
+	/* This should pass*/
+	sput_fail_if(1 == 0, "1 == 0");
 }
 
 int run_framework_tests(void)
