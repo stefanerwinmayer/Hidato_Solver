0d3d5f31653594f82b39cb69cd16b23712f17eb2
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2016-08-10 10:17:02 +0100

    Added stub for testing the sput framework.

---
+++ Tests/framework_tests.c
@@ -0,0 +1,27 @@
+#include "sput.h"
+
+
+static void test_fail_unless()
+{
+
+}
+
+static void test_fail_if()
+{
+
+}
+
+int rund_framework_tests(int argc, char *argv[])
+{
+	sput_start_testing();
+
+	sput_enter_suite("test_fail_unless()");
+	sput_run_test(test_fail_unless);
+
+	sput_enter_suite("test_fail_if()");
+	sput_run_test(test_fail_if);
+
+	sput_finish_testing();
+
+	return sput_get_return_value();
+}
