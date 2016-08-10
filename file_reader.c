45dfa80029b227013ccbd6ecbf3f5b44f3397a69
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2016-08-10 14:57:44 +0100

    Implemented file length function.

--- file_reader.c
+++ file_reader.c
@@ -1,6 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "file_reader.h"
 
 long file_length(const char *filename)
 {
-	return -1;
+	FILE *fp;
+	long pos;
+
+	if ((fp = fopen(filename, "r")) == NULL)
+	{
+		printf("Cannot open file %s\n", filename);
+		getchar();
+		exit(EXIT_FAILURE);
+	}
+
+	fseek(fp, 0L, SEEK_END);
+	pos = ftell(fp);
+
+	fclose(fp);
+	return pos;
 }
