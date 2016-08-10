3662dd31a9783d0fca2791898aa2fd1ffe3c0756
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2016-08-10 14:27:11 +0100

    Added test for empty input file.

--- Tests/file_reader_tests.c
+++ Tests/file_reader_tests.c
@@ -1,9 +1,17 @@
 #include "sput.h"
 #include "../file_reader.h"
 
+static void test_fail_unless()
+{
+	sput_fail_unless(file_length("test_empty_file.txt") == 0, "File length of empty file is 0");
+}
+
 int run_file_reader_tests(void)
 {
 	sput_start_testing();
+
+	sput_enter_suite("test_fail_unless()");
+	sput_run_test(test_fail_unless);
 
 	sput_finish_testing();
 
