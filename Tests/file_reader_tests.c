e7d1f126ed7ff7843e43bbd7ef18b6e1538501b5
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2016-08-10 15:33:45 +0100

    Added test for input file with 1 character.

--- Tests/file_reader_tests.c
+++ Tests/file_reader_tests.c
@@ -4,6 +4,7 @@
 static void test_file_length()
 {
 	sput_fail_unless(file_length("test_empty_file.txt") == 0, "File length of empty file should be 0");
+	sput_fail_unless(file_length("test_one_character_file.txt") == 1, "File length of file with 1 character should be 1");
 }
 
 int run_file_reader_tests(void)
