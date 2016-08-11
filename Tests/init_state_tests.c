51e7c9a5a6bafe0e5c22a6d13977da061f4e7503
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2016-08-11 12:29:03 +0100

    Testing input of a single number.

--- Tests/init_state_tests.c
+++ Tests/init_state_tests.c
@@ -1,10 +1,70 @@
 #include "sput.h"
 #include "../state_struct.h"
 #include "../init_state.h"
+#include "../coordinate_struct.h"
+
+struct State state_1;
+
+static void setup(void)
+{
+	initialise_state("[1]", &state_1);
+}
+
+static void test_initialise_state_board()
+{
+	sput_fail_unless(
+		state_1.board[0][0] == 'F',
+		"");
+}
+
+static void test_initialise_state_board_rows()
+{
+	sput_fail_unless(
+		state_1.board_rows == 1,
+		"");
+}
+
+static void test_initialise_state_board_cols()
+{
+	sput_fail_unless(
+		state_1.board_cols == 1,
+		"");
+}
+
+static void test_initialise_state_hamiltonian_rows()
+{
+	sput_fail_unless(
+		state_1.hamiltonian[0].row == 0,
+		"");
+}
+
+static void test_initialise_state_hamiltonian_cols()
+{
+	sput_fail_unless(
+		state_1.hamiltonian[0].col == 0,
+		"");
+}
 
 int run_init_state_tests(void)
 {
+	setup();
+
 	sput_start_testing();
+
+	sput_enter_suite("test_initialise_state_board()");
+	sput_run_test(test_initialise_state_board);
+
+	sput_enter_suite("test_initialise_state_board_rows()");
+	sput_run_test(test_initialise_state_board_rows);
+
+	sput_enter_suite("test_initialise_state_board_cols");
+	sput_run_test(test_initialise_state_board_cols);
+
+	sput_enter_suite("test_initialise_state_hamiltonian_rows()");
+	sput_run_test(test_initialise_state_hamiltonian_rows);
+
+	sput_enter_suite("test_initialise_state_hamiltonian_cols()");
+	sput_run_test(test_initialise_state_hamiltonian_cols);
 
 	sput_finish_testing();
 
