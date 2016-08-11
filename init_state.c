389e143cfbcd4a9d0a98b210d69a73aff9ad6dd7
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2016-08-11 15:16:31 +0100

    Implemented ability to produce state for one single digit number.

--- init_state.c
+++ init_state.c
@@ -1,7 +1,29 @@
 #include "init_state.h"
-#include "state_struct.h"
+
+#include <stdio.h>
 
 void initialise_state(char *input, struct State *state)
 {
+	
+	int row, col, number;
+	char *p;
 
+	state->board_rows = state->board_cols = 0;
+
+	row = col = 0;
+	
+	(state->board_rows)++;
+	for (p = input; p < input + strlen(input); p++)
+	{
+		if (isdigit(*p))
+		{
+			state->board[row][col] = 'F';
+			(state->board_cols)++;
+
+			number = *p - '0' - 1;
+			state->hamiltonian[number].row = row;
+			state->hamiltonian[number].col = col;
+		}
+		
+	}
 }
--- init_state.h
+++ init_state.h
@@ -1,6 +1,11 @@
 #ifndef STATE_BUILDER
 #define STATE_BUILDER
 
+#include <string.h>
+#include <ctype.h>
+#include "state_struct.h"
+#include "coordinate_struct.h"
+
 void initialise_state(char *input, struct State *state);
 
 #endif
