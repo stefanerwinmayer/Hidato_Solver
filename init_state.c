8a867fb05af399ee08fe526ceec0c83532fa81af
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2016-08-11 11:10:27 +0100

    Added stubs for state initialization.

---
+++ init_state.c
@@ -0,0 +1,7 @@
+#include "init_state.h"
+#include "state_struct.h"
+
+void initialise_state(char *input, struct State *state)
+{
+
+}
---
+++ init_state.h
@@ -0,0 +1,6 @@
+#ifndef STATE_BUILDER
+#define STATE_BUILDER
+
+void initialise_state(char *input, struct State *state);
+
+#endif
