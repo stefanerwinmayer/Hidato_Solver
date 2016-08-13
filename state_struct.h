d213679114ff16c5799a3538cebeac89743ec353
stefanem <stefan.e.mayer@gmail.com>
2016-08-13 12:38:39 +0100

    Modified state struct to use limits.

--- state_struct.h
+++ state_struct.h
@@ -1,11 +1,14 @@
 #ifndef STATE_STRUCT
 #define STATE_STRUCT
 
+#include "hidato_limits.h"
+#include "coordinate_struct.h"
+
 struct State {
-	struct Coordinate *hamiltonian;
+	struct Coordinate hamiltonian[MAX_NUMS];
 	int ham_length;
 
-	char **board;
+	char board[MAX_ROWS][MAX_COLS];
 	int board_rows;
 	int board_cols;
 };
