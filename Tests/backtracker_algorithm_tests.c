7d47cbe7bac1beabe8fe3a280774306bf893e0cd
stefanem <stefan.e.mayer@gmail.com>
2016-08-15 11:46:31 +0100

    Testing if a board of a single known number succeeds.

--- Tests/backtracker_algorithm_tests.c
+++ Tests/backtracker_algorithm_tests.c
@@ -1,9 +1,26 @@
 #include "sput.h"
+#include "../init_state.h"
 #include "../backtracker.h"
+
+struct State state_1;
+struct Coordinate start_1;
+
+static void test_backtrack_solvable()
+{
+	initialise_state("[1]", &state_1, &start_1);
+	
+	sput_fail_unless(
+		backtrack(&state_1, &start_1, 0),
+		"Single known node should succeed."
+	);
+}
 
 int run_backtracker_algorithm_tests(void)
 {
 	sput_start_testing();
+
+	sput_enter_suite("test_backtrack_solvable()");
+	sput_run_test(test_backtrack_solvable);
 
 	sput_finish_testing();
 
