572c5136b0e45918fefb1dc44db3bb29e1db50a0
stefanem <stefan.e.mayer@gmail.com>
2016-08-15 13:55:44 +0100

    Checking if impossible boards fail to backtrack.

--- Tests/backtracker_algorithm_tests.c
+++ Tests/backtracker_algorithm_tests.c
@@ -27,12 +27,25 @@ static void test_backtrack_solvable()
 	);
 }
 
+static void test_backtrack_unsolvable()
+{
+	initialise_state("[1][?][2]", &state, &start);
+
+	sput_fail_if(
+		backtrack(&state, &start, 0),
+		"Backtracking [1][?][2] should fail."
+	);
+}
+
 int run_backtracker_algorithm_tests(void)
 {
 	sput_start_testing();
 
 	sput_enter_suite("test_backtrack_solvable()");
 	sput_run_test(test_backtrack_solvable);
+
+	sput_enter_suite("test_backtrack_unsolvable()");
+	sput_run_test(test_backtrack_unsolvable);
 
 	sput_finish_testing();
 
