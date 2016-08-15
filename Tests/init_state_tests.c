0d16aae71d6aa63b6befb3d5531516e9162c6501
stefanem <stefan.e.mayer@gmail.com>
2016-08-15 11:00:36 +0100

    Checkinf if the start coordinate can be assigned when not in the first node.

--- Tests/init_state_tests.c
+++ Tests/init_state_tests.c
@@ -13,6 +13,7 @@ struct State state_6;
 struct State state_7;
 struct State state_8;
 struct State state_9;
+struct State state_10;
 
 struct Coordinate start_1 = { -1, -1 };
 struct Coordinate start_2 = { -1, -1 };
@@ -23,6 +24,7 @@ struct Coordinate start_6 = { -1, -1 };
 struct Coordinate start_7 = { -1, -1 };
 struct Coordinate start_8 = { -1, -1 };
 struct Coordinate start_9 = { -1, -1 };
+struct Coordinate start_10 = { -1, -1 };
 
 static void setup(void)
 {
@@ -43,6 +45,8 @@ static void setup(void)
 	initialise_state("[01][??]", &state_8, &start_8);
 
 	initialise_state("[01][??]\n[XX][02]", &state_9, &start_9);
+
+	initialise_state("[??][??]\n[01][??]", &state_10, &start_10);
 }
 
 static void test_initialise_state_board()
@@ -230,6 +234,11 @@ static void test_initialise_state_start_coordinate()
 		start_9.row == 0 &&
 		start_9.col == 0,
 		"Number 1 is at (0, 0)");
+
+	sput_fail_unless(
+		start_10.row == 1 &&
+		start_10.col == 0,
+		"Number 1 is at (1, 0)");
 }
 
 int run_init_state_tests(void)
