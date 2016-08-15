77c7e4a26f7ac45cc6a1f12589cd257fea0fbb0c
stefanem <stefan.e.mayer@gmail.com>
2016-08-15 14:24:17 +0100

    Implemented a function to print input/solution boards.

--- output.c
+++ output.c
@@ -1,4 +1,47 @@
+#include "output.h"
+
 void print_board(struct State *state)
 {
+	int i, j;
+	char output[MAX_ROWS][MAX_COLS] = { BLOCKED };
+	
+	for (i = 0; i < state->ham_length; i++)
+	{
+		if (state->hamiltonian[i].row >= 0 && state->hamiltonian[i].col >= 0)
+		{
+			output[state->hamiltonian[i].row][state->hamiltonian[i].col] = (char)i + 1;
+		}
+	}
 
+	for (i = 0; i < state->board_rows; i++)
+	{
+		for (j = 0; j < state->board_cols; j++)
+		{
+			if (state->board[i][j] == UNKNOWN)
+			{
+				output[i][j] = UNKNOWN;
+			}
+		}
+	}
+
+	for (i = 0; i < state->board_rows; i++)
+	{
+		for (j = 0; j < state->board_cols; j++)
+		{
+			if (output[i][j] != BLOCKED && output[i][j] != UNKNOWN)
+			{
+				printf("[%2.d]", output[i][j]);
+			}
+			else if (output[i][j] == BLOCKED)
+			{
+				printf("[XX]");
+			}
+			else
+			{
+				printf("[??]");
+			}
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
--- output.h
+++ output.h
@@ -1,6 +1,11 @@
 #ifndef OUTPUT
 #define OUTPUT
 
+#include <stdio.h>
 #include "state_struct.h"
+
+#define BLOCKED 'X'
+
+#define UNKNOWN -1
 
 #endif
