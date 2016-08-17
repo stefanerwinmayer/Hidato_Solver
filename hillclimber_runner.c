f1cba518e8f0f87e00a9a8bfb3c4ff7cbb241e41
stefanem <stefan.e.mayer@gmail.com>
2016-08-17 16:28:58 +0100

    Added state initialisation.

--- hillclimber_runner.c
+++ hillclimber_runner.c
@@ -2,6 +2,10 @@
 
 void run_hillclimber(char *input)
 {
+	struct HC_State state;
+
+	initialise_hillclimber(input, &state);
+
 	printf("Input Hidato:\n\n");
 
 	printf("Solving the puzzle with the hill climber algorithm:\n\n");
--- hillclimber_runner.h
+++ hillclimber_runner.h
@@ -2,6 +2,7 @@
 #define HILLCLIMBER_RUNNER
 
 #include <stdio.h>
+#include "init_state.h"
 
 void run_hillclimber(char *input);
 
