244c287e4255d34ebaba6a997d04a27d480994ea
stefanem <stefan.e.mayer@gmail.com>
2016-08-17 15:34:21 +0100

    Added a state struct for the hill climber.

---
+++ hillclimber_state_struct.h
@@ -0,0 +1,19 @@
+#ifndef HILLCLIMBER_STATE_STRUCT
+#define HILLCLIMBER_STATE_STRUCT
+
+#include "hidato_limits.h"
+#include "boolean.h"
+
+struct HC_State {
+	int number_count;
+
+	BOOL fixed[MAX_NUMS];
+
+	int solution[MAX_ROWS][MAX_COLS];
+	int best_solution[MAX_ROWS][MAX_COLS];
+
+	int board_rows;
+	int board_cols;
+}
+
+#endif
