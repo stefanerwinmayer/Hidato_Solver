a6593d5d9b9dd66df488eb681cf5630ffc2f8b45
stefanem <stefan.e.mayer@gmail.com>
2016-08-17 12:35:24 +0100

    Added conditional output depending on which backtracker is in use.

--- hidato_solver.c
+++ hidato_solver.c
@@ -43,17 +43,23 @@ int main(int argc, char *argv[])
 		printf("Input Hidato:\n\n");
 		print_board(&state);
 
+		printf("Solving the puzzle with the ");
+		(smart) ? printf("smart ") : printf("regular ");
+		printf("backtracker:\n\n");
+
 		state.backtrack_counter = 0;
 
 		backtrack(&state, state.hamiltonian, smart);
 
-		printf("Hamiltonian Path:\n");
-		print_hamiltonian_path(&state);
-
-		printf("Solved Hidato:\n\n");
 		print_board(&state);
 
-		printf("Number of calls to backtrack: %d", state.backtrack_counter);
+		printf("Hamiltonian Path:\n\n");
+		print_hamiltonian_path(&state);
+
+		printf("Number of calls to the ");
+		(smart) ? printf("smart ") : printf("regular ");
+		printf("backtracker: %d", state.backtrack_counter);
+	
 	}
 
 	(void)getchar();
