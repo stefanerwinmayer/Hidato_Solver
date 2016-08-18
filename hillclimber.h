a42d7e74261c850b7d2a97b1325cf430530456c0
stefanem <stefan.e.mayer@gmail.com>
2016-08-18 11:02:39 +0100

    Implemented the scoring system.

--- hillclimber.h
+++ hillclimber.h
@@ -2,8 +2,10 @@
 #define HILLCLIMBER
 
 #include "hillclimber_state_struct.h"
+#include "coordinate_struct.h"
 
 #define UNKNOWN -1
+#define NUMBER_OF_MOVES 8
 
 void produce_random_solution(struct HC_State *state);
 int assess_solution(struct HC_State *state);
