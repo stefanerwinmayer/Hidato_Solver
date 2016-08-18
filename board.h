fa60cead69a75296663fc29e69a604f66ec58bcd
stefanem <stefan.e.mayer@gmail.com>
2016-08-18 18:17:57 +0100

    Finished replacing parts of the state struct with the board struct.

--- board.h
+++ board.h
@@ -3,6 +3,12 @@
 
 #include "hidato_limits.h"
 
+#define FIXED -9
+#define VISITED_FIXED -8
+#define FREE -7
+#define TAKEN -6
+#define BLOCKED -5
+
 struct Board {
 	int grid[MAX_ROWS][MAX_COLS];
 
--- state_struct.h
+++ state_struct.h
@@ -4,12 +4,6 @@
 #include "hidato_limits.h"
 #include "coordinate_struct.h"
 
-#define FIXED -9
-#define VISITED_FIXED -8
-#define FREE -7
-#define TAKEN -6
-#define BLOCKED -5
-
 #define UNKNOWN -2
 
 struct State {
@@ -17,10 +11,6 @@ struct State {
 	int ham_length;
 
 	struct Coordinate *next_fixed;
-
-	int board[MAX_ROWS][MAX_COLS];
-	int board_rows;
-	int board_cols;
 
 	int backtrack_counter;
 };
