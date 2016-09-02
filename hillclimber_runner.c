90759aadde4192312607fc2d2adbef42702e674b
stefanem <stefan.e.mayer@gmail.com>
2016-09-02 15:12:44 +0100

    Modified output.

--- hillclimber_runner.c
+++ hillclimber_runner.c
@@ -5,22 +5,30 @@ void run_hillclimber(char *input)
 	struct Num_Coordinates initial;
 	struct Num_Coordinates best;
 	struct Board board;
-	int score;
+	int high_score;
+	int optimum_score;
 
 	time_t t;
 	srand((unsigned)time(&t));
 
 	initialise_state(input, &board, &initial);
+	optimum_score = initial.count - 1;
 
 	printf("Input Hidato:\n\n");
 	print_board(&board, &initial);
 
 	printf("Solving the puzzle with the hill climber algorithm:\n\n");
 
-	score = climb_hills(&board, &initial, &best);
+	high_score = climb_hills(&board, &initial, &best);
 	
 	printf("Best solution:\n\n");
 	print_board(&board, &initial);
 
-	printf("Score: %d", score);
+	printf("Best possible score: %d\n", optimum_score);
+	printf("Achieved Score: %d\n", high_score);
+	if (high_score != optimum_score)
+	{
+		printf("NOT ");
+	}
+	printf("SOLVED!");
 }
