98a509d6c655bfe9325fc6d26cb85c6dc8b6dd24
stefanem <stefan.e.mayer@gmail.com>
2016-09-03 12:48:30 +0100

    Added benchmarking for windows.

--- backtracker_runner.c
+++ backtracker_runner.c
@@ -1,3 +1,7 @@
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #include "backtracker_runner.h"
 
 void run_backtracker(
@@ -8,16 +12,31 @@ void run_backtracker(
 	struct Num_Coordinates numbers;
 	int counter = 0;
 
+#ifdef _WIN32
+	LARGE_INTEGER frequency;
+	LARGE_INTEGER t1, t2;
+	double elapsedTime;
+#endif
+
 	initialise_state(input, &board, &numbers);
 
 	printf("Input Hidato:\n\n");
 	print_board(&board, &numbers);
-
 	printf("Solving the puzzle with the ");
 	(smart) ? printf("smart ") : printf("regular ");
 	printf("backtracker:\n\n");
 
+#ifdef _WIN32
+	QueryPerformanceFrequency(&frequency);
+	QueryPerformanceCounter(&t1);
+#endif
+
 	backtrack(&board, &numbers, numbers.coordinates, &counter, smart);
+
+#ifdef _WIN32
+	QueryPerformanceCounter(&t2);
+	elapsedTime = (t2.QuadPart - t1.QuadPart) * 1000.0 / frequency.QuadPart;
+#endif
 
 	print_board(&board, &numbers);
 
@@ -26,5 +45,9 @@ void run_backtracker(
 
 	printf("Number of calls to the ");
 	(smart) ? printf("smart ") : printf("regular ");
-	printf("backtracker: %d", counter);
+	printf("backtracker: %d\n", counter);
+
+#ifdef _WIN32
+	printf("Runtime: %lf ms", elapsedTime);
+#endif
 }
