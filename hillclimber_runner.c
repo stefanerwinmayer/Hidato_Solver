609c7c324b45a65eecce2146edaa54c808194690
stefanem <stefan.e.mayer@gmail.com>
2016-09-03 12:53:20 +0100

    Added benchmarking for windows.

--- hillclimber_runner.c
+++ hillclimber_runner.c
@@ -1,3 +1,7 @@
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #include "hillclimber_runner.h"
 
 void run_hillclimber(char *input)
@@ -11,6 +15,12 @@ void run_hillclimber(char *input)
 	time_t t;
 	srand((unsigned)time(&t));
 
+#ifdef _WIN32
+	LARGE_INTEGER frequency;
+	LARGE_INTEGER t1, t2;
+	double elapsedTime;
+#endif
+
 	initialise_state(input, &board, &initial);
 	optimum_score = initial.count - 1;
 
@@ -19,7 +29,17 @@ void run_hillclimber(char *input)
 
 	printf("Solving the puzzle with the hill climber algorithm:\n\n");
 
+#ifdef _WIN32
+	QueryPerformanceFrequency(&frequency);
+	QueryPerformanceCounter(&t1);
+#endif
+
 	high_score = climb_hills(&board, &initial, &best);
+
+#ifdef _WIN32
+	QueryPerformanceCounter(&t2);
+	elapsedTime = (t2.QuadPart - t1.QuadPart) * 1000.0 / frequency.QuadPart;
+#endif
 
 	printf("Best solution:\n\n");
 	print_board(&board, &best);
@@ -27,4 +47,8 @@ void run_hillclimber(char *input)
 	printf("Best possible score: %d\n", optimum_score);
 	printf("Achieved Score: %d\n", high_score);
 	printf("Quality of solution: %.2lf %%\n", ((double)high_score / optimum_score) * 100);
+
+#ifdef _WIN32
+	printf("Runtime: %lf ms", elapsedTime);
+#endif
 }
