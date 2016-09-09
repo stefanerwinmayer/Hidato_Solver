880ff78dbe8966fe466f1380da8e65464c47db9d
stefanem <stefan.e.mayer@gmail.com>
2016-09-09 20:27:33 +0100

    Moving init state into the main function.

--- hillclimber_runner.c
+++ hillclimber_runner.c
@@ -4,14 +4,12 @@
 
 #include "hillclimber_runner.h"
 
-void run_hillclimber(char *input)
+void run_hillclimber(
+	struct Board *board,
+	struct Num_Coordinates *initial)
 {
-	struct Num_Coordinates initial;
-	struct Board board;
-	int i;
+	int high_score;
 	int optimum_score;
-	int high_score = 0;
-	int points[MAX_NUMS] = { 0 };
 
 	time_t t;
 	srand((unsigned)time(&t));
@@ -22,10 +20,10 @@ void run_hillclimber(char *input)
 	double elapsedTime;
 #endif
 
-	initialise_state(input, &board, &initial);
+	optimum_score = initial->count - 1;
 
 	printf("Input Hidato:\n\n");
-	print_board(&board, &initial);
+	print_board(board, initial);
 
 	printf("Solving the puzzle with the hill climber algorithm:\n\n");
 
@@ -34,7 +32,7 @@ void run_hillclimber(char *input)
 	QueryPerformanceCounter(&t1);
 #endif
 
-	climb_hills(&board, &initial, points);
+	high_score = climb_hills(board, initial);
 
 #ifdef _WIN32
 	QueryPerformanceCounter(&t2);
@@ -42,16 +40,9 @@ void run_hillclimber(char *input)
 #endif
 
 	printf("Best solution:\n\n");
-	print_board(&board, &initial);
+	print_board(board, initial);
 
-	optimum_score = initial.count - 1;
 	printf("Best possible score: %d\n", optimum_score);
-
-	for (i = 0; i < initial.count; i++)
-	{
-		high_score += points[i];
-	}
-
 	printf("Achieved Score: %d\n", high_score);
 	printf("Quality of solution: %.2lf %%\n", ((double)high_score / optimum_score) * 100);
 
