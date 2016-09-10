5017f3806bd217467794bbf8c8ac9f119afa905d
stefanem <stefan.e.mayer@gmail.com>
2016-09-10 10:33:38 +0100

    Updated Test project

--- Tests/test_runner.c
+++ Tests/test_runner.c
@@ -4,8 +4,8 @@
 int main(void)
 {
 	if (!run_framework_tests() &&
-		!run_file_reader_tests() &&
-		!run_init_state_tests()) //&&
+		!run_file_reader_tests())// &&
+		//!run_init_state_tests()) //&&
 		//!run_backtracker_tests() &&
 		//!run_backtracker_algorithm_tests())
 	{
