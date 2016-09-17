73600f1cb8e71d529a8fa8a075710d93caa51b34
stefanem <stefan.e.mayer@gmail.com>
2016-09-17 12:26:24 +0100

    Modified test

--- Tests/backtracker_functions_tests.c
+++ Tests/backtracker_functions_tests.c
@@ -49,6 +49,7 @@ static void test_update_hamiltonian()
 	struct Coordinate neighbour_fixed = { 3, 3 };
 
 	numbers.coordinates[1].row = UNKNOWN; numbers.coordinates[1].col = UNKNOWN;
+
 	numbers.coordinates[2].row = 3; numbers.coordinates[2].col = 3;
 
 	update_hamiltonian(numbers.coordinates + 1, &neighbour_guess);
@@ -68,17 +69,18 @@ static void test_revert_hamiltonian()
 	struct Board board;
 	struct Num_Coordinates numbers;
 
-	board.grid[0][0] = FIXED;
-	board.grid[0][1] = FREE;
-	numbers.coordinates[0].row = 0; numbers.coordinates[0].col = 0;
-	numbers.coordinates[1].row = 0; numbers.coordinates[1].col = 1;
+	board.grid[2][2] = FIXED;
+	numbers.coordinates[0].row = 2; numbers.coordinates[0].col = 2;
+
+	board.grid[3][3] = FREE;
+	numbers.coordinates[1].row = 3; numbers.coordinates[1].col = 3;
 
 	revert_hamiltonian(&board, numbers.coordinates);
 	revert_hamiltonian(&board, numbers.coordinates + 1);
 
 	sput_fail_unless(
-		numbers.coordinates[0].row == 0 &&
-		numbers.coordinates[0].col == 0 &&
+		numbers.coordinates[0].row == 2 &&
+		numbers.coordinates[0].col == 2 &&
 		numbers.coordinates[1].row == UNKNOWN &&
 		numbers.coordinates[1].col == UNKNOWN,
 		"Fixed (0, 0) should stay as is, (0, 1) -> UNKNOWN"
