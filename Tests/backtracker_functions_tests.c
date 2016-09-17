104486bfc16e3494be87e4e20caf80a8fe5818cb
stefanem <stefan.e.mayer@gmail.com>
2016-09-17 11:32:37 +0100

    Added test to check the update board functionality.

--- Tests/backtracker_functions_tests.c
+++ Tests/backtracker_functions_tests.c
@@ -1,16 +1,32 @@
 #include "sput.h"
 #include "../backtracker.h"
 
-static void test_distance()
+static void test_update_board()
 {
+	struct Board board;
+	struct Num_Coordinates numbers;
+
+	board.grid[0][0] = FIXED;
+	board.grid[0][1] = FREE;
+	numbers.coordinates[0].row = 0; numbers.coordinates[0].col = 0;
+	numbers.coordinates[1].row = 0; numbers.coordinates[1].col = 1;
+
+	update_board(&board, numbers.coordinates);
+	update_board(&board, numbers.coordinates + 1);
+
+	sput_fail_unless(
+		board.grid[0][0] == VISITED_FIXED &&
+		board.grid[0][1] == TAKEN,
+		"FIXED -> VISITED_FIXED, FREE -> TAKEN"
+	);
 }
 
 int run_backtracker_functions_tests(void)
 {
 	sput_start_testing();
 
-	sput_enter_suite("test_distance()");
-	sput_run_test(test_distance);
+	sput_enter_suite("test_update_board()");
+	sput_run_test(test_update_board);
 
 	sput_finish_testing();
 
