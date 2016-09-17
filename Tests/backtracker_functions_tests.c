c9691ae5f1c1ffbf96b0a4ac05782a9284d7e9b1
stefanem <stefan.e.mayer@gmail.com>
2016-09-17 12:22:29 +0100

    Modified test

--- Tests/backtracker_functions_tests.c
+++ Tests/backtracker_functions_tests.c
@@ -6,17 +6,18 @@ static void test_update_board()
 	struct Board board;
 	struct Num_Coordinates numbers;
 
-	board.grid[0][0] = FIXED;
-	board.grid[0][1] = FREE;
-	numbers.coordinates[0].row = 0; numbers.coordinates[0].col = 0;
-	numbers.coordinates[1].row = 0; numbers.coordinates[1].col = 1;
+	board.grid[2][2] = FIXED;
+	numbers.coordinates[0].row = 2; numbers.coordinates[0].col = 2;
+	
+	board.grid[3][3] = FREE;
+	numbers.coordinates[1].row = 3; numbers.coordinates[1].col = 3;
 
 	update_board(&board, numbers.coordinates);
 	update_board(&board, numbers.coordinates + 1);
 
 	sput_fail_unless(
-		board.grid[0][0] == VISITED_FIXED &&
-		board.grid[0][1] == TAKEN,
+		board.grid[2][2] == VISITED_FIXED &&
+		board.grid[3][3] == TAKEN,
 		"FIXED -> VISITED_FIXED, FREE -> TAKEN"
 	);
 }
