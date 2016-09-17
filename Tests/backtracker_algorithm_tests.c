037a2530ed2e2aeeb205f0306aa9b15fc1cb78d4
stefanem <stefan.e.mayer@gmail.com>
2016-09-17 15:29:43 +0100

    Added backtracking test puzzle 2

--- Tests/backtracker_algorithm_tests.c
+++ Tests/backtracker_algorithm_tests.c
@@ -38,12 +38,49 @@ static void test_backtrack_test_puzzle_1()
 	);
 }
 
+static void test_backtrack_test_puzzle_2()
+{
+	char input[MAX_FILE_LENGTH];
+	struct Board board;
+	struct Num_Coordinates numbers;
+	int iterations = 0;
+
+	file_to_string("test_puzzle_2.txt", input);
+	initialise_state(input, &board, &numbers);
+
+	sput_fail_unless(
+		backtrack(&board, &numbers, numbers.coordinates, &iterations, FALSE) == TRUE,
+		"Valid puzzle -> should return TRUE"
+	);
+
+	sput_fail_unless(
+		board.grid[0][0] == VISITED_FIXED && board.grid[0][1] == VISITED_FIXED && board.grid[0][2] == BLOCKED &&
+		board.grid[1][0] == TAKEN && board.grid[1][1] == TAKEN && board.grid[1][2] == BLOCKED &&
+		board.grid[2][0] == VISITED_FIXED && board.grid[2][1] == TAKEN && board.grid[2][2] == VISITED_FIXED,
+		"All nodes have the correct state after backtracking"
+	);
+
+	sput_fail_unless(
+		numbers.coordinates[0].row == 2 && numbers.coordinates[0].col == 2 &&
+		numbers.coordinates[1].row == 1 && numbers.coordinates[1].col == 1 &&
+		numbers.coordinates[2].row == 2 && numbers.coordinates[2].col == 1 &&
+		numbers.coordinates[3].row == 2 && numbers.coordinates[3].col == 0 &&
+		numbers.coordinates[4].row == 1 && numbers.coordinates[4].col == 0 &&
+		numbers.coordinates[5].row == 0 && numbers.coordinates[5].col == 1 &&
+		numbers.coordinates[6].row == 0 && numbers.coordinates[6].col == 0,
+		"All numbers have the correct coordinates"
+	);
+}
+
 int run_backtracker_algorithm_tests(void)
 {
 	sput_start_testing();
 
 	sput_enter_suite("test_backtrack_test_puzzle_1");
 	sput_run_test(test_backtrack_test_puzzle_1);
+
+	sput_enter_suite("test_backtrack_test_puzzle_2");
+	sput_run_test(test_backtrack_test_puzzle_2);
 
 	sput_finish_testing();
 
