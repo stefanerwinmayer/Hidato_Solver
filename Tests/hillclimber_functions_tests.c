ff1addbf75fb1f978c8498a97bb41cb00a7fc37d
stefanem <stefan.e.mayer@gmail.com>
2016-09-18 12:25:22 +0100

    Added tests to check update points function

--- Tests/hillclimber_functions_tests.c
+++ Tests/hillclimber_functions_tests.c
@@ -86,19 +86,12 @@ static void test_find_number_to_swap()
 
 static void test_swap_numbers()
 {
-	struct Board board;
 	struct Num_Coordinates solution;
-
-	board.grid[0][0] = FIXED; board.grid[0][1] = TAKEN;
-	board.grid[1][0] = TAKEN; board.grid[1][1] = FIXED;
-	board.grid[2][0] = BLOCKED; board.grid[2][1] = BLOCKED;
-	board.rows = 3; board.cols = 2;
 
 	solution.coordinates[0].row = 0; solution.coordinates[0].col = 0;
 	solution.coordinates[1].row = 0; solution.coordinates[1].col = 1;
 	solution.coordinates[2].row = 1; solution.coordinates[2].col = 1;
 	solution.coordinates[3].row = 1; solution.coordinates[3].col = 0;
-	solution.count = 4;
 
 	swap_numbers(&solution, 1, 3);
 
@@ -108,6 +101,36 @@ static void test_swap_numbers()
 		solution.coordinates[2].row == 1 && solution.coordinates[2].col == 1 &&
 		solution.coordinates[3].row == 0 && solution.coordinates[3].col == 1,
 		"Number coordinates are swapped correctly, rest is untouched."
+	);
+}
+
+static void test_update_points()
+{
+	struct Num_Coordinates solution;
+	int points[MAX_NUMS] = { 0 };
+
+	solution.coordinates[0].row = 0; solution.coordinates[0].col = 0;
+	solution.coordinates[1].row = UNKNOWN; solution.coordinates[1].col = UNKNOWN;
+	solution.coordinates[2].row = 1; solution.coordinates[2].col = 1;
+	solution.coordinates[3].row = UNKNOWN; solution.coordinates[3].col = UNKNOWN;
+
+	update_points(&solution, points, 0, 4);
+
+	sput_fail_unless(
+		points[0] == 0 && points[1] == 0 && points[2] == 0 && points[3] == 0,
+		"Initial puzzle has no points."
+	);
+
+	solution.coordinates[0].row = 0; solution.coordinates[0].col = 0;
+	solution.coordinates[1].row = 0; solution.coordinates[1].col = 1;
+	solution.coordinates[2].row = 1; solution.coordinates[2].col = 1;
+	solution.coordinates[3].row = 1; solution.coordinates[3].col = 0;
+
+	update_points(&solution, points, 0, 4);
+
+	sput_fail_unless(
+		points[0] == 1 && points[1] == 1 && points[2] == 1 && points[3] == 0,
+		"Solved puzzle has all possible points."
 	);
 }
 
@@ -123,6 +146,9 @@ int run_hillclimber_functions_tests(void)
 
 	sput_enter_suite("test_swap_numbers()");
 	sput_run_test(test_swap_numbers);
+
+	sput_enter_suite("test_udpate_points()");
+	sput_run_test(test_update_points);
 
 	sput_finish_testing();
 
