e6b380f726478740481333f2d63b38616681c594
stefanem <stefan.e.mayer@gmail.com>
2016-09-18 12:05:13 +0100

    Added tests to check find numbers to swap function

--- Tests/hillclimber_functions_tests.c
+++ Tests/hillclimber_functions_tests.c
@@ -5,7 +5,6 @@ static void test_produce_random_solution()
 {
 	struct Board board;
 	struct Num_Coordinates numbers;
-	int seen[4] = { FALSE };
 
 	board.grid[0][0] = FIXED; board.grid[0][1] = FREE;
 	board.grid[1][0] = FREE; board.grid[1][1] = FIXED;
@@ -38,8 +37,50 @@ static void test_produce_random_solution()
 			numbers.coordinates[3].row == 1 && numbers.coordinates[3].col == 0) ||
 
 			(numbers.coordinates[1].row == 1 && numbers.coordinates[1].col == 0 &&
-				numbers.coordinates[3].row == 0 && numbers.coordinates[3].col == 1),
+				numbers.coordinates[3].row ==  0 && numbers.coordinates[3].col == 1),
 		"Remaining numbers have unique coordinates assigned."
+	);
+}
+
+static void test_find_number_to_swap()
+{
+	struct Board board;
+	struct Num_Coordinates solution;
+
+	board.grid[0][0] = FIXED; board.grid[0][1] = TAKEN;
+	board.grid[1][0] = TAKEN; board.grid[1][1] = FIXED;
+	board.grid[2][0] = BLOCKED; board.grid[2][1] = BLOCKED;
+	board.rows = 3; board.cols = 2;
+
+	solution.coordinates[0].row = 0; solution.coordinates[0].col = 0;
+	solution.coordinates[1].row = 0; solution.coordinates[1].col = 1;
+	solution.coordinates[2].row = 1; solution.coordinates[2].col = 1;
+	solution.coordinates[3].row = 1; solution.coordinates[3].col = 0;
+	solution.count = 4;
+
+	sput_fail_unless(
+		find_number_to_swap(&board, &solution, 0) == 1,
+		"Number 2 at index 1 should be found"
+	);
+
+	sput_fail_unless(
+		find_number_to_swap(&board, &solution, 1) == 1,
+		"Number 2 at index 1 should be found"
+	);
+
+	sput_fail_unless(
+		find_number_to_swap(&board, &solution, 2) == 3,
+		"Number 4 at index 3 should be found"
+	);
+
+	sput_fail_unless(
+		find_number_to_swap(&board, &solution, 3) == 3,
+		"Number 4 at index 3 should be found"
+	);
+
+	sput_fail_unless(
+		find_number_to_swap(&board, &solution, 4) == 4,
+		"No further number can be found"
 	);
 }
 
@@ -49,6 +90,9 @@ int run_hillclimber_functions_tests(void)
 
 	sput_enter_suite("test_produce_random_solution()");
 	sput_run_test(test_produce_random_solution);
+
+	sput_enter_suite("test_find_number_to_swap()");
+	sput_run_test(test_find_number_to_swap);
 
 	sput_finish_testing();
 
