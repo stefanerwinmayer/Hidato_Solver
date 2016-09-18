e638a24e49a9573b1b083a6ef545b1180d5a8b5c
stefanem <stefan.e.mayer@gmail.com>
2016-09-18 21:32:59 +0100

    Added define

--- distance.h
+++ distance.h
@@ -1,6 +1,11 @@
+#ifndef DISTANCE
+#define DISTANCE
+
 #include <math.h>
 #include "coordinate_struct.h"
 
 int distance(
 	const struct Coordinate *first,
 	const struct Coordinate *second);
+
+#endif
